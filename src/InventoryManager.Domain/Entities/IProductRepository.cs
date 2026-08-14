d9f0753f12ac19e1e898d517c0dc5ad0eeae6065
Leonardo Marinho <leowalkg6@Gmail.com>
2026-08-13 22:32:27 -0300

    Criando interface IProductRepository

---
+++ src/InventoryManager.Domain/Entities/IProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace InventoryManager.Domain.Entities
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken = default);
+
+        Task<List<Product>> GetAllAsync(
+            CancellationToken cancellationToken = default);
+            
+
+        Task AddAsync (
+            Product product, CancellationToken cancellationToken = default);
+
+        void Update(Product product);
+        void Delete(Product product);
+
+        Task<bool> ExistsAsync(
+            Guid id, CancellationToken cancellationToken = default);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
